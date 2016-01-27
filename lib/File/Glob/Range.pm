a2e34d629b6b89cc55d494ea023f93c46992a2f1
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2016-01-27 15:12:42 +0000

    Initial Commit

---
+++ lib/File/Glob/Range.pm
@@ -0,0 +1,18 @@
+package File::Glob::Range;
+# ABSTACT: this is the module abstract
+
+=head1 SYNOPSIS
+
+use File::Glob::Range;
+my @some_info='here;
+
+=head1 DESCRIPTION
+
+A description about the module
+
+=cut
+
+use strict;
+use warnings;
+
+1;
